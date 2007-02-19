26ad8690606ff5aa46d112e72ed002303bbd18c9
mbchandar <mbchandar@4b7b552b-c329-0410-aabe-ed1c012e2f92>
2007-02-19 15:33:00 +0000

    added new supplier table

--- WindowsApplication2/My Project/Application.Designer.vb
+++ WindowsApplication2/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.InventorManagement.frmMain
+            Me.MainForm = Global.InventorManagement.frmProducts
         End Sub
     End Class
 End Namespace
